cd0a928f58218bfdfc9b9c320e7f2c9ec42b1d13
angelaignatoska <ignatoskaangela10@gmail.com>
2026-04-18 22:30:35 +0200

    Update SupremeDecisionRepository

---
+++ src/main/java/com/example/agents_evaluation/Repository/SupremeDecisionRepository.java
@@ -0,0 +1,15 @@
+Angela — 10:26 PM
+package com.example.agents_evaluation.Repository;
+
+import com.example.agents_evaluation.Model.SupremeDecision;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SupremeDecisionRepository extends JpaRepository<SupremeDecision, Long> {
+
+    @Override
+    Optional<SupremeDecision> findById(Long aLong);
+}
